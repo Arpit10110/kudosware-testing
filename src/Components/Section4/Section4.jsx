5310dcb8026e3387446093ec67e4d6de15c9525e
Arpit <omagrahari55@gmail.com>
2024-10-21 19:25:05 +0530

    slider fuctionality done

--- src/Components/Section4/Section4.jsx
+++ src/Components/Section4/Section4.jsx
@@ -1,63 +1,91 @@
-import React from 'react'
-import "./Section4.css"
-//img
-import  sec4Img from "../../assets/sec4Img.png"
-import sec4box1 from "../../assets/sec4box1.png"
-import sec4box2 from "../../assets/sec4box2.png"
-import sec4box3 from "../../assets/sec4box3.png"
-const Section4 = () => {
-  return (
-   <>
-   <div className="section4" id='rent'>
-    <h2>Why Rent?</h2>
-    <div className="sec3btn-div">
-        <button>Cost Saving</button>
-        <button>Environment Saving</button>
-        <button>Space Saving</button>
-    </div>
-    <div className="doyouknow">
-        <div className="sec4-cont">
-            <h3>DID YOU KNOW?</h3>
-            <p>Babies typically outgrow 6 or more sizes in their first year! Add in 3 different seasons, special occasions, and several outfit changes daily, now that's a LOT of clothes!</p>
-            <img src={sec4Img} alt="sec4Img" />
-            <div className="sec4btn-div">
-                <button>AVERAGE SPENT: $250</button>
-                <button>TINYCLO: $32</button>
-            </div>
-        </div>
-    </div>
-    <div className="section3-ContBox">
-        <div className="sec3box">
-            <div className="sec3box-img">
-                <img src={sec4box1} alt="sec3box1" />
-            </div>
-            <div className="secbox-cont">
-                <h3>SWAP ANYTIME</h3>
-                <p>When you're ready to swap, we'll send new looks right away.your previous looks using our pre-paid, reusable shipping kit. It’s that easy!</p>
-            </div>
-        </div>
-        <div className="sec3box">
-            <div className="sec3box-img">
-                <img src={sec4box2} alt="sec3box1" />
-            </div>
-            <div className="secbox-cont">
-                <h3>CURATE & SHIP</h3>
-                <p>Pick a plan and share your style preferences. We’ll customize and ship your little’s capsule within 7 business days.</p>
-            </div>
-        </div>
-        <div className="sec3box">
-            <div className="sec3box-img">
-                <img src={sec4box3} alt="sec3box1" />
-            </div>
-            <div className="secbox-cont">
-                <h3>GROW IN STYLE</h3>
-                <p>Dress your little in their mix-and-match wardrobe as long as you'd like. When you're ready to swap, switch one item or everything. It's you!</p>
-            </div>
-        </div>
-    </div>
-   </div>
-   </>
-  )
-}
+import React, { useState } from 'react';
+import "./Section4.css";
+// Images
+import sec4box1 from "../../assets/sec4box1.png";
+import sec4box2 from "../../assets/sec4box2.png";
+import sec4box3 from "../../assets/sec4box3.png";
+import SliderCompo from "../Slider/Slider.jsx";
 
-export default Section4
+const Section4 = () => {
+    const [selectedButton, setSelectedButton] = useState("costSaving");
+
+    // Button click handlers
+    const handleCostSavingClick = () => {
+        setSelectedButton("costSaving");
+    };
+
+    const handleEnvSavingClick = () => {
+        setSelectedButton("envSaving");
+    };
+
+    const handleSpaceSavingClick = () => {
+        setSelectedButton("spaceSaving");
+    };
+
+    return (
+        <div className="section4" id='rent'>
+            <h2>Why Rent?</h2>
+            <div className="sec3btn-div">
+                <button 
+                    onClick={handleCostSavingClick}
+                    style={{ backgroundColor: selectedButton === "costSaving" ? "#FCEBA3" : "#D9D9D9" }}
+                >
+                    Cost Saving
+                </button>
+                <button 
+                    onClick={handleEnvSavingClick}
+                    style={{ backgroundColor: selectedButton === "envSaving" ? "#FCEBA3" : "#D9D9D9" }}
+                >
+                    Environment Saving
+                </button>
+                <button 
+                    onClick={handleSpaceSavingClick}
+                    style={{ backgroundColor: selectedButton === "spaceSaving" ? "#FCEBA3" : "#D9D9D9" }}
+                >
+                    Space Saving
+                </button>
+            </div>
+            {selectedButton === "costSaving" && 
+                <SliderCompo cont={"Babies typically outgrow 6 or more sizes in their first year! Add in 3 different seasons, special occasions, and several outfit changes daily, now that's a LOT of clothes!"} />
+            }
+            {selectedButton === "envSaving" && 
+                <SliderCompo cont={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur voluptate accusantium eveniet sunt aliquid molestias a dolorum necessitatibus, inventore odit?"} />
+            }
+            {selectedButton === "spaceSaving" && 
+                <SliderCompo cont={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur voluptate accusantium eveniet sunt aliquid molestias a dolorum necessitatibus, inventore odit?"} />
+            }
+
+            <div className="section3-ContBox">
+                <div className="sec3box">
+                    <div className="sec3box-img">
+                        <img src={sec4box1} alt="Swap Anytime" />
+                    </div>
+                    <div className="secbox-cont">
+                        <h3>SWAP ANYTIME</h3>
+                        <p>When you're ready to swap, we'll send new looks right away using our pre-paid, reusable shipping kit. It’s that easy!</p>
+                    </div>
+                </div>
+                <div className="sec3box">
+                    <div className="sec3box-img">
+                        <img src={sec4box2} alt="Curate & Ship" />
+                    </div>
+                    <div className="secbox-cont">
+                        <h3>CURATE & SHIP</h3>
+                        <p>Pick a plan and share your style preferences. We’ll customize and ship your little’s capsule within 7 business days.</p>
+                    </div>
+                </div>
+                <div className="sec3box">
+                    <div className="sec3box-img">
+                        <img src={sec4box3} alt="Grow in Style" />
+                    </div>
+                    <div className="secbox-cont">
+                        <h3>GROW IN STYLE</h3>
+                        <p>Dress your little in their mix-and-match wardrobe as long as you'd like. When you're ready to swap, switch one item or everything. It's you!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Section4;
---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import './VariationSlider.css';
+import sliderbackground from "../../assets/sliderbackground.png";
+
+const SliderCompo = ({cont,Slidevalue}) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  // Handle slider change
+  const handleSliderChange = (value) => {
+    setSelectedIndex(value);
+  };
+
+  const ageGroups = [
+    { age: '0-3 months', AverageSpent: "340", TinyClo: "50" },
+    { age: '3-6 months', AverageSpent: "330", TinyClo: "60" },
+    { age: '6-9 months', AverageSpent: "320", TinyClo: "70" },
+    { age: '9-12 months', AverageSpent: "310", TinyClo: "80" },
+    { age: '12-18 months', AverageSpent: "370", TinyClo: "20" },
+    { age: '18-24 months', AverageSpent: "380", TinyClo: "40" },
+  ];
+
+  return (
+    <div className="slider-container">
+        <div className="sec4-cont">
+            <h3>DID YOU KNOW?</h3>
+            <p>{cont}</p>
+        </div>
+      <div className="slider">
+        <Slider
+          min={0}
+          max={ageGroups.length - 1}
+          value={selectedIndex}
+          marks={ageGroups.reduce((acc, group, index) => {
+            acc[index] = group.age;
+            return acc;
+          }, {})}
+          onChange={handleSliderChange}
+          dotStyle={{ display: 'none' }}
+          handleStyle={{
+            borderColor: '#fda8a8',
+            height: "5vw",
+            width: "5vw",
+            marginLeft: "-1rem",
+            marginTop: "-3rem",
+            backgroundImage: `url(${sliderbackground})`,
+            backgroundSize: 'cover',
+            opacity: "100%"
+          }}
+          trackStyle={{ backgroundColor: '#64c3b8' }}
+          railStyle={{ backgroundColor: '#f0f0f0' }}
+        />
+      </div>
+      <div className="sec4btn-div">
+        <button>AVERAGE SPENT: ${ageGroups[selectedIndex].AverageSpent}</button>
+        <button>TINYCLO: ${ageGroups[selectedIndex].TinyClo}</button>
+      </div>
+    </div>
+  );
+};
+
+export default SliderCompo;
